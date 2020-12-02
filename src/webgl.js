0028f56e95a627476bcea57d8320644a73727db1
Gregg King <gregg.king@redspace.com>
2020-12-02 03:51:56 -0400

    Simplify initCopyProgram for readability and a possible performance gain.
    Branches can harm performance so removed the multiple branches of the if-else. Setting the copyOut to a default value is cheap so we always do that with no checking. We run one if with &&s which allows breaking out early if any of the conditions fail. If all pass we do the expensive operation (compared to setting a vec4) of sampling the texture.

--- src/webgl.js
+++ src/webgl.js
@@ -183,13 +183,15 @@ function initCopyProgram(gl) {
   layout(location = 0) out vec4 copyOut;
 
   void main() {
-    if (gl_FragCoord[0] < renderArea.x) {
-      copyOut = vec4(0.0, 0.0, 0.0, 1.0);
-    } else if (gl_FragCoord[0] > renderArea.z) {
-      copyOut = vec4(0.0, 0.0, 0.0, 1.0);
-    } else if ((gl_FragCoord[1] < renderArea.y) || (gl_FragCoord[1] > renderArea.w)) {
-      copyOut = vec4(0.0, 0.0, 0.0, 1.0);
-    } else {
+    // default return black
+    copyOut = vec4(0.0, 0.0, 0.0, 1.0);
+
+    // if we are not in the padding around the video, get the texture value
+    if (gl_FragCoord.x > renderArea.x
+      && gl_FragCoord[0] < renderArea.z
+      && gl_FragCoord[1] > renderArea.y
+      && gl_FragCoord[1] < renderArea.w
+    ) {
       copyOut = texture(originalSampler, vec2((gl_FragCoord[0] - renderArea.x) / videoRes.x, 1.0 - ((gl_FragCoord[1] - renderArea.y) / videoRes.y)));
     }
   }
